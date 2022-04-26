28ac0900d097a7db075f0ddd4fae0ffdaf0e10d9
learnuidev <learnuidev@gmail.com>
2022-04-25 22:16:11 -0400

    remove style and add to git to reduxe noise

--- src/cljs/app/view.cljs
+++ src/cljs/app/view.cljs
@@ -22,13 +22,14 @@
   (let [is-alert-open (r/atom false)]
     (fn []
       [:div.m-8
+       [:> Alert {:message "You lost, the word was"
+                  :isOpen @is-alert-open}]
        [:nav.mb-8
         [:h1.font-600 "novus"]]
        [:div.my-16
         [:h1.uppercase "Where Kids learn to"
          [:strong.block "solve problems together"]]
-        [:> Alert {:message "You lost, the word was"
-                   :isOpen @is-alert-open}]
+
         [:> Button {:title (if @is-alert-open "Close" "Open")
                     :onClick #(reset! is-alert-open (not @is-alert-open))}]
         [:f> custom-hooks-counter]]])))
