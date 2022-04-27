3b5d5fac28431b788394e6e623da156fa967f3df
learnuidev <learnuidev@gmail.com>
2022-04-26 23:35:07 -0400

    added more content

--- src/cljs/app/view.cljs
+++ src/cljs/app/view.cljs
@@ -18,10 +18,10 @@
 (defn nav-bar []
   [:nav.mb-8.flex.justify-between.items-stretch
    [:h1.font-600 "novus"]
-   [:div.flex.items-stretch
-     [:div.flex.items-center.h-full.-space-x-6
-      [:div.flex.items-center
-       [:p "courses"]]]]])
+   [:div.flex.grid-gap-2
+    [:p.mx-4 "courses"]
+    [:p.mx-4 "login"]
+    [:p.mx-4 "register"]]])
 
 (defn splash-page []
   [:div.my-16
@@ -31,23 +31,77 @@
    [:div.my-8
     [:h3.text-2xl.font-light "novus challenges and pushes curious learners through "
      [:strong.font-medium "real life "
-      [:span.block " startup simulations and complex startup games"]]]
+      [:span.block " startup simulations and complex games"]]]
     [:> Button {:title "Get Started"
                 :className "px-8"}]]])
 
 
 (defn why-novus []
-  [:div.my-16.grid.place-items-center
-   [:h1.uppercase.text-2xl "Why novus?"]])
-
-
+  [:div.my-16
+   [:div.grid.place-items-center
+    [:h1.uppercase.text-2xl "Our Learning Goals"]]
+   [:p.italic.my-8 "The key to mastery and long term skills is focusing on principals. We designed our curriculum with the following learning goals."]
+   [:div.grid.grid-cols-2.gap-4.place-items-center
+    [:p "focus on " [:span.font-bold "principles"] ", not " [:span.italic "\"best practices\""]]
+    [:p "build a strong learning foundation"]
+    [:p "practice " [:span.font-bold "error driven development"]]
+    [:p "learn like a " [:span.font-bold "mechanic"]]
+    [:p "learn to solve problems  " [:span.font-bold "collaboratively"]]
+    [:p "soratic method - learn to ask the " [:span.font-bold "right questions"]]
+    [:p "be curious, be " [:span.font-bold "self critical"]]
+    [:p "become code literate - master the art of reading any codebase"]
+    [:p "learn to " [:span.font-bold "think in data"]]
+    [:p "learn to " [:span.font-bold "thinking in/about systems"]]]])
 
 (defn fundamentals []
   [:div.my-16
    [:div.grid.place-items-center
-    [:h1.uppercase.font-800.text-2xl "Core Foundations"]
+    [:h1.uppercase.font-800.text-2xl "Core Foundations 🎓"]
     [:h3.my-4.italic "this series is about mastering the fundamentals of web development by hacking your own duolingo account, interactively"]
-    [:p "skills you will pick up: " [:strong "html, javascript, css, how to learn, value oriented programming, object oriented programming, how to think in data, flow driven development, learn how to learn, hacking, google chrome browser, events"]]]])
+    [:p "skills you will pick up: " [:strong "html, javascript, css, how to learn, value oriented programming, object oriented programming, how to think in data, flow driven development, learn how to learn, hacking, google chrome browser, events, how to think like a programmer, question everything"]]]
+   [:div.grid.gap-8.grid-cols-2.my-8
+    [:div
+     [:h4.text-xl "Module 1 - Computing: Past, Present and Future 🎓"]
+     [:p "spicyness: " [:strong "🌶️🌶️🌶️🌶️"]]
+     [:p "difficulty: " [:strong "😀 💪"]]
+     [:p "prequesites: " [:strong " none"]]
+     [:p "time: " [:strong "2~4 months"]]]
+    [:div
+     [:h4.text-xl "Module 2 - What is Software Engineering?"]
+     [:p "spicyness: " [:strong "🌶️🌶️🌶️🌶️"]]
+     [:p "difficulty: " [:strong "😀 💪"]]
+     [:p "prequesites: " [:strong " none"]]
+     [:p "time: " [:strong "2~4 months"]]]
+    [:div
+     [:h4.text-xl "Module 3 - HTML and CSS"]
+     [:p "spicyness: " [:strong "🌶️🌶️🌶️🌶️"]]
+     [:p "difficulty: " [:strong "😀 💪"]]
+     [:p "prequesites: " [:strong " none"]]
+     [:p "time: " [:strong "2~4 months"]]]
+    [:div
+     [:h4.text-xl "Module 4 - JavaScript"]
+     [:p "spicyness: " [:strong "🌶️🌶️🌶️🌶️"]]
+     [:p "difficulty: " [:strong "😀 💪"]]
+     [:p "prequesites: " [:strong " none"]]
+     [:p "time: " [:strong "2~4 months"]]]
+    [:div
+     [:h4.text-xl "Module 5 - Thinking in Data: Introduction"]
+     [:p "spicyness: " [:strong "🌶️🌶️🌶️🌶️"]]
+     [:p "difficulty: " [:strong "😀 💪"]]
+     [:p "prequesites: " [:strong " none"]]
+     [:p "time: " [:strong "2~4 months"]]]
+    [:div
+     [:h4.text-xl "Module 6 - Networking 101"]
+     [:p "spicyness: " [:strong "🌶️🌶️🌶️🌶️"]]
+     [:p "difficulty: " [:strong "😀 💪"]]
+     [:p "prequesites: " [:strong " none"]]
+     [:p "time: " [:strong "2~4 months"]]]
+    [:div
+     [:h4.text-xl "Module 7 - Database 101"]
+     [:p "spicyness: " [:strong "🌶️🌶️🌶️🌶️"]]
+     [:p "difficulty: " [:strong "😀 💪"]]
+     [:p "prequesites: " [:strong " none"]]
+     [:p "time: " [:strong "2~4 months"]]]]])
 
 (defn startup-simulations []
   [:div.my-16
@@ -72,6 +126,7 @@
      [:p "time: " [:strong "4 months"]]]
     [:div
      [:h4.text-xl "Internship 3 - Athens Research -" [:strong "software engineer"]]
+     [:p "spicyness: " [:strong "🌶️🌶️🌶️🌶️"]]
      [:p "difficulty: " [:strong "💪"]]
      [:p "prequesites: " [:strong " 💪💪"]]
      [:p "tech stack: " [:strong " clojure, clojurescript & typescript"]]
@@ -93,8 +148,9 @@
 (defn capstone-project []
   [:div.my-16
    [:div.grid.place-items-center
-    [:h1.uppercase.font-800.text-2xl "Capstone Project"]
-    [:h3.my-4.italic.font-bold "this series simulates what a typical university student their software engineering intership journey "]]])
+    [:h1.uppercase.font-800.text-2xl "Capstone Project - Building own startup"]
+    [:h3.my-4.italic.font-bold "this series simulates what a typical university student their software engineering intership journey "]
+    [:p.my-4 "Duration: " [:strong "6 months"]]]])
 
 
 (defn main-view []
