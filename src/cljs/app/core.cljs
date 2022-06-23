8874b679184b49fac2c9932c46527e96382a06cf
learnuidev <learnuidev@gmail.com>
2022-06-22 23:02:07 -0400

    moved amplify to its own

--- src/cljs/app/core.cljs
+++ src/cljs/app/core.cljs
@@ -2,13 +2,22 @@
   "This namespace contains your application and is the entrypoint for 'yarn start'."
   (:require [reagent.core :as r]
             [reagent.dom :as rdom]
-            [app.config]
+            ;; aws
+            ["/aws/amplify" :as amplify]
+            [app.config :as config]
             [app.auth]
             ;; Layout
             [app.layout :refer [app]]
             ;; router
             [app.routes :refer [router-start!]]))
             ; [app.view :refer [main-view]]))
+
+;; configure aws
+(amplify/configure (clj->js config/env))
+
+;; whats inside env
+(comment
+  (identity config/env))
 
 (defn ^:dev/after-load render
   "Render the toplevel component for this app."
